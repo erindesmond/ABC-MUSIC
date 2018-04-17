e13fae478efca56be4d1fd3597df65f196411ae5
Erin Desmond <erindesmond@Erins-MacBook-Pro.local>
2018-04-17 16:11:39 -0600

    model sort of working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# abc-music
+# LSTMusic
+##### Traditional Irish Music with a Modern Twist
+
+
+"Never get one of those cheap tin whistles. It leads to much harder drugs like pipes and flutes."
--- abc-lstm.py
+++ abc-lstm.py
@@ -1,104 +1,119 @@
-import os
 import tensorflow as tf
-import re
-import collections
+import numpy as np
 import pdb
 
-'cool'
-# Consider removing some of the words from the text:
-# perhaps anything that says X:, S:, or % Nottingham music database
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.wrappers import TimeDistributed
 
 
-# Concatenate all the abc files together
-# I then manually split into 10% test and 90% train
-# This resulted in 850 train songs and 94 test songs
-def concatFiles():
-    '''Takes a path that contains files to be concatenated, outputs one text file
-    with all files together. Only run once!'''
+def load_data(filename):
 
-    path = './nottingham-dataset/ABC_cleaned/'
-    files = os.listdir(path)
-    for idx, infile in enumerate(files):
-        print ("File #" + str(idx) + "  " + infile)
-    concat = ''.join([open(path + f).read() for f in files])
-    with open("abc_all.txt", "w") as fo:
-        fo.write(path + concat)
-
-
-def read_measures(filename):
     with tf.gfile.GFile(filename, 'r') as f:
 
         data = f.read()
-        print(data)
-        measures = data.split(r"|")
-        return measures
-        #return f.read().replace('|', '<eos>').split()
+        characters = list(set(data))
+        vocab_size = len(characters)
 
-def build_vocab(filename):
-    data = read_measures(filename)
+    return data, characters, vocab_size
 
-    counter = collections.Counter(data)
-    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+def create_idx_dictionary(characters):
 
-    words, _ = list(zip(*count_pairs))
-    word_to_id = dict(zip(words, range(len(words))))
+        char_to_idx = {char:idx for idx,char in enumerate(characters)}
+        idx_to_char = {idx:char for idx,char in enumerate(characters)}
 
-    return word_to_id
+        return char_to_idx, idx_to_char
 
-def file_to_word_ids(filename, word_to_id):
+def prepare_X_y(data, len_sequences, num_sequences, num_features, char_to_idx_dict):
 
-    data = read_words(filename)
-    return [word_to_id[word] for word in data if word in word_to_id]
+    X = np.zeros((len(data)//len_sequences, len_sequences, num_features))
+    y = np.zeros((len(data)//len_sequences, len_sequences, num_features))
 
-def load_data():
-    # get the data paths
+    for i in range(0, int(num_sequences)):
 
-    train_path = os.path.join('./', "abc_train.txt")
-    test_path = os.path.join('./', "abc_test.txt")
+        # assign the sequence in chosen length from data to X_sequence
+        # indicate the index for that sequence in the dict from above, assign to list
+        X_sequence = data[i*len_sequences:(i+1)*len_sequences]
+        X_sequence_idx = [char_to_idx_dict[value] for value in X_sequence]
 
-    # build the complete vocabulary, then convert text data to list of integers
-    word_to_id = build_vocab(train_path)
-    train_data = file_to_word_ids(train_path, word_to_id)
-    test_data = file_to_word_ids(test_path, word_to_id)
-    vocabulary = len(word_to_id)
-    reversed_dictionary = dict(zip(word_to_id.values(), word_to_id.keys()))
+        # Create empty matrix of zeros for the input sequences to arrange by idx
+        # This allows us to 'one-hot-encode' based on where that sequence happened
+        # If that sequence is in that index, set equal to 1
+        input_sequence = np.zeros((len_sequences, num_features))
+        for j in range(len_sequences):
+            input_sequence[j][X_sequence_idx[j]] = 1.
 
-    print(train_data[:5])
-    print(word_to_id)
-    print(vocabulary)
-    print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
-    return train_data, test_data, vocabulary, reversed_dictionary
+    X[i] = input_sequence
+
+    # target sequence will be same as X, but taking one step forward. This is to
+    # set the target by shifting the corresponding input sequence
+    # by one character. (So it's predicting on the next character)
+    # indicate the index for that sequence from the dict from above, assign to list
+    y_sequence = data[i*len_sequences+1:(i+1)*len_sequences+1]
+    y_sequence_idx = [char_to_idx_dict[value] for value in y_sequence]
+
+    # here we set the target sequence by indexing into y with the y_sequence (which is
+    # a step ahead of X, remember)
+    target_sequence = np.zeros((len_sequences, num_features))
+    for j in range(len_sequences):
+        target_sequence[j][y_sequence_idx[j]] = 1.
+    y[i] = target_sequence
+
+    return X, y
+
+def generate_text(model, length):
+    idx = [np.random.randint(vocab_size)]
+    y_char = [idx_to_char_dict[idx[-1]]]
+    X = np.zeros((1, length, vocab_size))
+
+    for i in range(length):
+
+        X[0, i, :][idx[-1]] = 1
+        print(idx_to_char_dict[idx[-1]], end="")
+        idx = np.argmax(model.predict(X[:, :i+1, :])[0], 1)
+        y_char.append(idx_to_char_dict[idx[-1]])
+    return ('').join(y_char)
+
+if __name__ == '__main__':
+
+    data, characters, vocab_size = load_data('abc_all.txt')
+    char_to_idx_dict, idx_to_char_dict = create_idx_dictionary(characters)
+
+    len_sequences = 10
+    num_sequences = int(len(data) / len_sequences)
+    num_features = vocab_size
+
+    X, y = prepare_X_y(data, len_sequences, num_sequences, num_features, char_to_idx_dict)
 
 
+    dropout = 0.3
+    internal_size = 2
 
 
+    model = Sequential()
 
+    number_of_layers = 2
+    for layer in range(number_of_layers - 1):
 
-if __name__ == "__main__":
-    #concatFiles()
-    train_data, test_data, vocabulary, reversed_dict = load_data()
+        model.add(LSTM(internal_size,input_shape=(None, vocab_size),
+                       return_sequences=True))
 
+    model.add(Dropout(dropout))
+    model.add(TimeDistributed(Dense(vocab_size), kernel_initializer='random_uniform'))
+    model.add(Activation('softmax'))
 
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
+    batch_size = 10
+    generate_length = 50
 
+    num_epoch = 0
+    while True:
+        print('\n\n')
+        model.fit(X, y, batch_size=batch_size, verbose=1, epochs=1)
+        num_epoch += 1
+        generate_text(model, generate_length)
 
-    # f = open('abc_train.txt')
-    #
-    # for word in f.read().split():
-    #     print(word)
-
-
-
-
-
-
-
-    # def load_data():
-    #     train_path = os.path.join(./ABC-LSTM/, "abc_train.txt")
-    #     test_path = os.path.join(./ABC-LSTM/, "abc_test.txt")
-    #
-    #     word_to_id = build_vocab
-    #     train_data = file_to_word_ids
-    #     test_data =
-    #     abc_vocab =
-    #     reverse_dict =
+        if num_epoch % 10 == 0:
+            model.save_weights('checkpoint_{}_epoch{}.hdf5'.format(internal_size, num_epoch))
