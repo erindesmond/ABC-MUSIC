4ece7680ab9a07b5f17f4731bf1fbc9888674e58
Erin Desmond <erindesmond@Erins-MacBook-Pro.local>
2018-04-18 20:05:58 -0600

    first results

--- README.md
+++ README.md
@@ -22,13 +22,23 @@ Therefore, for this particular project, my data is limited to simpler melodic so
 
 ##### The Model
 
-I am using a [Keras LSTM](https://keras.io/layers/recurrent/#lstm) model with only one LSTM cell that has 10 memory units, a batch size of 20, a sequence size of 25, a time step of 3, a dropout rate of 0.3, an RMSprop optimizer with a learning rate of 0.01, and final softmax activation function.
+I am using a [Keras LSTM](https://keras.io/layers/recurrent/#lstm) model with only one LSTM cell that has 10 memory units with a dropout rate of 0.3, an RMSprop optimizer with a learning rate of 0.01, and final softmax activation function.
 
 This model randomly chooses a string of 25 characters, and computes the probability distribution of these characters. Then through a function that normalizes the distributions by numbers that represent the standard deviation from the mean of that distribution, it randomly picks a character to append to the original 25 characters, at which point it takes a step forward in time, dropping the first character from the original string. In this way, it makes an 'educated' guess as to what the next character should be. As weights are updated at the end of each batch size, the model will learn probabilities over time. But because of the random choice from the distribution of the original 25 characters, the predicted text will not exactly match the original text.
 
 ##### Tensorboard Images     
 
 ##### Results
+After 1 epoch with a batch size of 100:
+![one_epoch](images/one_epoch.png)
+
+After 10 epochs:
+![ten_epoch](images/ten_epochs.png)
+
+After 20 epochs:
+![twenty_epochs](images/twenty_epochs.png)
+
+
 
 ##### References
 1. [The Unreasonable Effectiveness of Recurrent Neural Networks](http://karpathy.github.io/2015/05/21/rnn-effectiveness/)
--- again.py
+++ again.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 from keras.utils import np_utils
@@ -10,7 +11,7 @@ from keras.layers.recurrent import LSTM
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.wrappers import TimeDistributed
 from keras.optimizers import RMSprop
-from keras.callbacks import LambdaCallback
+from keras.callbacks import LambdaCallback, TensorBoard
 
 
 def load_data(filename):
@@ -31,12 +32,12 @@ def create_idx_dictionary(characters):
 
         return char_to_idx, idx_to_char
 
-def prepare_X_y(data, num_char, sequences, time_step, char_to_idx_dict):
+def prepare_X_y(data, num_char, sequences, char_to_idx_dict):
 
     dataX = []
     dataY = []
 
-    for idx in range(0, num_char-sequences, time_step):
+    for idx in range(0, num_char-sequences, 1):
         sequence_in = data[idx:idx + sequences]
         next_chars = data[idx + sequences]
         dataX.append(sequence_in)
@@ -46,10 +47,10 @@ def prepare_X_y(data, num_char, sequences, time_step, char_to_idx_dict):
 
     return dataX, dataY, num_samples
 
-def vectorize_X_y(dataX, sequences, num_chars, char_to_idx_dict, next_chars):
+def vectorize_X_y(dataX, sequences, characters, char_to_idx_dict, next_chars):
 
-    X = np.zeros((len(dataX), sequences, num_chars), dtype=np.bool)
-    y = np.zeros((len(dataX), num_chars), dtype=np.bool)
+    X = np.zeros((len(dataX), sequences, len(characters)), dtype=np.bool)
+    y = np.zeros((len(dataX), len(characters)), dtype=np.bool)
 
     for idx, pattern in enumerate(dataX):
         for jdx, char in enumerate(pattern):
@@ -58,57 +59,71 @@ def vectorize_X_y(dataX, sequences, num_chars, char_to_idx_dict, next_chars):
 
     return X, y
 
-def model(X, y, num_chars):
+def model(X, y, characters):
 
     memory_units = 10
     dropout_rate = 0.3
     optimizer = RMSprop(lr=0.01)
 
     model = Sequential()
-    model.add(LSTM(memory_units, input_shape=(sequences, num_chars))) #return_sequences=true # 1 label per timestep
-    #model.add(Dropout(dropout_rate))
-    model.add(Dense(num_chars, activation='softmax'))
+    model.add(LSTM(memory_units, input_shape=(sequences, len(characters)))) #return_sequences=true if you want more LSTMS
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(len(characters), activation='softmax'))
 
 
     model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
     return model
 
-def sample(preds, temperature=1.0):
+def sample_from_distribution(preds, diversity):
     # helper function to sample an index from a probability array
+    # essentially, this normalizes the predicted probablility distribution numbers
+    # by dividing them by the diversity argument. This further randomizes the choosing
+    # process so that we don't generate the exact same text that we trained on
+
     preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
+    preds = np.log(preds) / diversity
     exp_preds = np.exp(preds)
     preds = exp_preds / np.sum(exp_preds)
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
 def on_epoch_end(epoch, logs):
+
+    data = load_data('abc_test.txt')[0]
+    characters = load_data('abc_test.txt')[1]
+    sequences = 25
+    idx_to_char_dict = create_idx_dictionary(characters)[1]
+    char_to_idx_dict = create_idx_dictionary(characters)[0]
     # Function invoked at end of each epoch. Prints generated text.
     print()
     print('----- Generating text after Epoch: %d' % epoch)
 
-    start_index = random.randint(0, len(data) - sequences - 1)
+    start_index = np.random.randint(0, len(data)-sequences - 1)
+    # how far to deviate away from the probability distribution mean, further makes more random
+
     for diversity in [0.2, 0.5, 1.0, 1.2]:
         print('----- diversity:', diversity)
 
-        generated = ''
+        # grab the first length of text from the random start index in len(sequences)
+        music_generated = ''
         pattern = data[start_index: start_index + sequences]
-        generated += pattern
+        music_generated += pattern
         print('----- Generating with seed: "' + pattern + '"')
-        sys.stdout.write(generated)
+        sys.stdout.write(music_generated)
 
         for i in range(400):
-            x_pred = np.zeros((1, sequences, len(chars)))
+            x_pred = np.zeros((1, sequences, len(characters)))
             for t, char in enumerate(pattern):
                 x_pred[0, t, char_to_idx_dict[char]] = 1.
 
-            preds = model.predict(x_pred, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_char = indices_char[next_index]
 
-            generated += next_char
-            sentence = sentence[1:] + next_char
+            preds = model.predict(x_pred, verbose=0)[0]
+            next_index = sample_from_distribution(preds, diversity)
+            next_char = idx_to_char_dict[next_index]
+
+            music_generated += next_char
+            pattern = pattern[1:] + next_char
 
             sys.stdout.write(next_char)
             sys.stdout.flush()
@@ -118,22 +133,25 @@ def on_epoch_end(epoch, logs):
 if __name__ == '__main__':
 
     sequences = 25
-    time_step = 1
 
-    data, characters, num_chars, vocab_size = load_data('abc_test.txt')
+    data, characters, num_chars, vocab_size = load_data('abc_all.txt')
 
     char_to_idx, idx_to_char = create_idx_dictionary(characters)
 
-    dataX, dataY, num_samples = prepare_X_y(data, num_chars, sequences, time_step, char_to_idx)
+    dataX, dataY, num_samples = prepare_X_y(data, num_chars, sequences, char_to_idx)
 
-    X, y = vectorize_X_y(dataX, sequences, num_chars, char_to_idx, dataY) #dayaY = next_char
+    X, y = vectorize_X_y(dataX, sequences, characters, char_to_idx, dataY) #dayaY = next_char
 
+    model = model(X, y, characters)
+    batch_size = 100
+
+    tensor_callback = tensorboard = TensorBoard(log_dir='./logs', batch_size=batch_size, write_graph=True, write_grads=True, write_images=True)
     print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 
-    model = model(X, y, num_chars)
+
     model.fit(X, y, epochs=20,
-              batch_size=20,
-              callbacks=[print_callback])
+              batch_size=batch_size,
+              callbacks=[print_callback, tensor_callback])
 
 
 
